6306845fa162f3364fa5567a7db740a39531afc4
akalmannakarmi <akalnakarmi1@gmail.com>
2023-09-10 14:45:37 +0545

    top body

--- js/base.js
+++ js/base.js
@@ -1,5 +1,15 @@
+fetch('details/index/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById("my-navbar").innerHTML=data;
+});
 fetch('details/myinfo/intro.html')
     .then(response => response.text())
     .then(data => {
     document.getElementById("my-info").innerHTML=data;
+});
+fetch('details/myinfo/projects.html')
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById("my-projects").innerHTML=data;
 });
